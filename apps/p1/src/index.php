b60cd09e75338501a2e30582453c150816ab8630
Tomasz Paździurek <tomasz.pazdziurek.stud@pw.edu.pl>
2022-05-24 22:34:24 +0200

    feat(favicon): set up favicon

--- apps/p1/src/index.php
+++ apps/p1/src/index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <title>PHP Test</title>
+    <link rel="icon" type="image/svg" href="assets/book-icon.svg"/>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
